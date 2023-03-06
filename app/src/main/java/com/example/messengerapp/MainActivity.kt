1ee52f83feefadb3f023db5ecf372601c2da1cc7
Anastasia <sevrukovaanastasia832@gmail.com>
2023-03-06 23:25:00 +0200

    fragments

--- app/src/main/java/com/example/messengerapp/MainActivity.kt
+++ app/src/main/java/com/example/messengerapp/MainActivity.kt
@@ -1,52 +1,81 @@
 package com.example.messengerapp
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.ViewPager
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import com.example.messengerapp.Fragments.MessengerFragment
+import com.example.messengerapp.Fragments.SearchFragment
+import com.example.messengerapp.Fragments.SettingsFragment
 import com.example.messengerapp.databinding.ActivityMainBinding
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
-import com.google.firebase.auth.PhoneAuthCredential
+import com.google.android.material.tabs.TabLayout
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(binding.toolbarMain)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        val toolbar: Toolbar = findViewById(R.id.toolbar_main)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.title = ""
 
-        setSupportActionBar(binding.toolbar)
+        val tabLayout: TabLayout = findViewById(R.id.tab_layout)
+        val viewPager: ViewPager = findViewById(R.id.view_pager)
+        val viewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
 
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        viewPagerAdapter.addFragment(MessengerFragment(), "Messenger")
+        viewPagerAdapter.addFragment(SearchFragment(), "Search")
+        viewPagerAdapter.addFragment(SettingsFragment(), "Settings")
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        viewPager.adapter = viewPagerAdapter
+        tabLayout.setupWithViewPager(viewPagerAdapter)
+
+    }
+    //val navController = findNavController(R.id.nav_host_fragment_content_main)
+    //appBarConfiguration = AppBarConfiguration(navController.graph)
+    //setupActionBarWithNavController(navController, appBarConfiguration)
+
+
+    internal class ViewPagerAdapter(fragmentManager: FragmentManager) :
+        FragmentPagerAdapter(fragmentManager) {
+
+        private val fragments: ArrayList<Fragment>
+        private val titles: ArrayList<String>
+
+        init {
+            fragments = ArrayList<Fragment>()
+            titles = ArrayList<String>()
+        }
+
+        override fun getCount(): Int {
+            return fragments.size
+        }
+
+        override fun getItem(position: Int): Fragment {
+            return fragments[position]
+        }
+
+        fun addFragment(fragment: Fragment, title: String) {
+            fragments.add(fragment)
+            titles.add(title)
+        }
+
+        override fun getPageTitle(i: Int): CharSequence {
+            return titles[i]
         }
     }
-
-
-    private fun signInWithPhoneAuthCredential(credential: PhoneAuthCredential) {
-        FirebaseAuth.getInstance().signInWithCredential(credential)
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    Timber.v("signInWithCredential:success ${taskId.result?.user?.uid}")
-                } else {
-                    Timber.v("signInWithCredential:failure ${task.exception}")
-                    if (task.exception is FirebaseAuthInvalidCredentialsException)
-                        context.toast("Invalid verification code!")
-                }
-            }
 }
+
+
+
